9a2ee375ce74eedf14f62427177043a28c07cd61
Tom Hanson <thanson@and.digital>
2020-08-13 09:44:15 +0100

    updated favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,7 @@ module.exports = {
         background_color: fullConfig.theme.colors.white,
         theme_color: fullConfig.theme.colors.paper,
         display: `minimal-ui`,
-        icon: `src/images/tailwind-icon.png`,
+        icon: `src/images/favicon.png`,
       },
     },
     {
